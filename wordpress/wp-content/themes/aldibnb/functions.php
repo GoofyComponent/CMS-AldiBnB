fa0a66918a2f50f10d54787b2e574625918d87ce
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2022-03-04 18:10:46 +0100

    Add landing page

--- wordpress/wp-content/themes/aldibnb/functions.php
+++ wordpress/wp-content/themes/aldibnb/functions.php
@@ -1,5 +1,28 @@
 <?php 
+    function aldibnb_setup(){
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_theme_support('menus'); 
+    }
 
-    add_theme_support('title-tag');
+    function aldibnb_menus(){
+        $locations = array(
+            'landing-nav' => 'Navigation landing page',
+            'footer-nav' => 'Navigation footer'
+        );
 
+        register_nav_menus($locations);
+    }
+
+    add_action('init', 'aldibnb_menus');
+
+    function aldibnb_register_styles(){
+        $version = wp_get_theme()->get('Version');
+        wp_enqueue_style('aldibnb-style', get_template_directory_uri() . '/style.css', array(), $version, 'all');
+        wp_enqueue_style('landing', get_template_directory_uri() . '/assets/styles/landing.css', array(), $version , 'all');
+        wp_enqueue_style( 'font-awesome-free', 'https://use.fontawesome.com/releases/v6.0.0/css/all.css' );
+    }
+    
+    add_action('after_setup_theme', 'aldibnb_setup');
+    add_action('wp_enqueue_scripts', 'aldibnb_register_styles');
 ?>
--- wordpress/wp-content/themes/aldibnb/header.php
+++ wordpress/wp-content/themes/aldibnb/header.php
@@ -1,12 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
 
 <body>
+    <header>
+        <div>
+            <h1>ALDI'NB</h1>
+
+        </div>
+        <nav>
+            <?php 
+            wp_nav_menu(array(
+                'menu' => 'landing-nav',
+                'theme_location' => 'landing-nav',
+                'container' => '',
+                'menu_class' => 'nav-menu',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>'
+            ));
+        ?>
+        </nav>
+    </header>
+
     <div id="container">
